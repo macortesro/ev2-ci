b888ff469cec478d819be286571b5af15d021c9f
macortesro <matiascortes2398@gmail.com>
2025-08-30 14:26:06 -0400

    test: agregar pruebas atomicas de suma y resta

---
+++ src/test/java/com/macortes/RestaTest.java
@@ -0,0 +1,10 @@
+package com.macortes;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class RestaTest {
+  @Test
+  void resta_dos_enteros() {
+    assertEquals(1, 5 - 4);
+  }
+}
---
+++ src/test/java/com/macortes/SumaTest.java
@@ -0,0 +1,10 @@
+package com.macortes;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class SumaTest {
+  @Test
+  void suma_dos_enteros() {
+    assertEquals(7, 3 + 4);
+  }
+}
