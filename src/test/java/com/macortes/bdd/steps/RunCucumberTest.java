b882142b1752f9e0534a980d6976743c99e90293
macortesro <matiascortes2398@gmail.com>
2025-08-31 21:20:54 -0400

    test: configura runner de Cucumber con JUnit 5

--- src/test/java/com/macortes/bdd/steps/RunCucumberTest.java
+++ src/test/java/com/macortes/bdd/steps/RunCucumberTest.java
@@ -1,21 +1,8 @@
 package com.macortes.bdd.steps;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import io.cucumber.junit.platform.engine.Cucumber;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME;
-
-@Suite
-@IncludeEngines("cucumber")
-@SelectClasspathResource("features") // mira src/test/resources/features
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.macortes.bdd.steps")
-@ConfigurationParameter(
-        key = PLUGIN_PROPERTY_NAME,
-        value = "pretty, html:target/cucumber-report.html, json:target/cucumber-report.json"
-)
-@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "not @wip")
-public class RunCucumberTest { }
+@Cucumber
+public class RunCucumberTest {
+    // No necesita código extra: esta clase es solo el runner.
+}
