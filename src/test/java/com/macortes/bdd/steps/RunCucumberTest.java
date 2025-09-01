ac4fff8ae64525d13333a4d6eb149470d542629a
macortesro <matiascortes2398@gmail.com>
2025-08-31 21:05:14 -0400

    test(bdd): runner JUnit Platform + glue + reportes

--- src/test/java/com/macortes/bdd/steps/RunCucumberTest.java
+++ src/test/java/com/macortes/bdd/steps/RunCucumberTest.java
@@ -1,17 +1,21 @@
-package com.macortes.bdd;
+package com.macortes.bdd.steps;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME;
 
 @Suite
-@SelectClasspathResource("features")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.macortes.bdd")
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features") // mira src/test/resources/features
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.macortes.bdd.steps")
 @ConfigurationParameter(
-  key = PLUGIN_PROPERTY_NAME,
-  value = "pretty, html:target/cucumber-report.html, json:target/cucumber-report.json"
+        key = PLUGIN_PROPERTY_NAME,
+        value = "pretty, html:target/cucumber-report.html, json:target/cucumber-report.json"
 )
+@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "not @wip")
 public class RunCucumberTest { }
